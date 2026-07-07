10556f47c2a36ed25310f5c306085c7de1246e85
taylaahmed <14827@holbertonstudents.com>
2026-07-08 06:36:13 +1000

    07

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - checks if a random number is positive, negative, or zero
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char c;
+
+for (c = 'z'; c >= 'a'; c--)
+{
+putchar(c);
+}
+
+putchar('\n');
+
+return (0);
+}
