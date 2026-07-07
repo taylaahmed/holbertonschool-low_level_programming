6e888c6e2e132061eaf9e37fa4fdfc9e59550781
taylaahmed <14827@holbertonstudents.com>
2026-07-08 06:14:03 +1000

    04

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 /**
-* main - Entry point
-*
-* Return: 0
-*/
+ * main - checks if a random number is positive, negative, or zero
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
 char c;
 
-for (c = 'a'; c<= 'z'; c++)
+for (c = 'a'; c <= 'z'; c++)
 {
 if (c != 'q' && c != 'e')
 putchar(c);
