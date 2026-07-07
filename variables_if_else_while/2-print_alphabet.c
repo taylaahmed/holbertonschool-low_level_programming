e46c5a866421f919914a2cf87510bedc5109841f
taylaahmed <14827@holbertonstudents.com>
2026-07-08 06:09:49 +1000

    02

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
+/**
+ * main - checks if a random number is positive, negative, or zero
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
 char c;
 
-for(c = 'a'; c<= 'z'; c++)
+for (c = 'a'; c <= 'z'; c++)
 {
 putchar(c);
 }
