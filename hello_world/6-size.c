a2f2fc408aa9fa9096430b93aa5128f6f3467a2d
taylaahmed <14827@holbertonstudents.com>
2026-07-07 16:11:36 +0000

    06

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -14,11 +14,11 @@ long int l;
 long long int e;
 float f;
 
-printf("Size of char: %d byte(s)\n", (int)sizeof(c));
-printf("Size of int: %d byte(s)\n", (int)sizeof(i));
-printf("Size of long int: %d byte(s)\n", (int)sizeof(l));
-printf("Size of long long int: %d byte(s)\n", (int)sizeof(e));
-printf("Size of float: %d byte(s)\n", (int)sizeof(f));
+printf("Size of a char: %d byte(s)\n", (int)sizeof(c));
+printf("Size of an int: %d byte(s)\n", (int)sizeof(i));
+printf("Size of a long int: %d byte(s)\n", (int)sizeof(l));
+printf("Size of a long long int: %d byte(s)\n", (int)sizeof(e));
+printf("Size of a float: %d byte(s)\n", (int)sizeof(f));
 
 return (0);
 }
