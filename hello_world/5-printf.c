bab72b9502c1c9a1a3578f279feeb41b4e976667
taylaahmed <14827@holbertonstudents.com>
2026-07-08 01:12:59 +1000

    C01 - 05

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+/* entry point - main
+*
+* return 0 - success
+*/
 
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,/n");
-return(0);
+return (0);
 }
