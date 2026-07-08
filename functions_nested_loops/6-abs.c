6d984b1cb3d610e31120b3604f9c45103a1df8e9
taylaahmed <14827@holbertonstudents.com>
2026-07-09 05:35:43 +1000

    05

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _abs - print plus, zero, negetive
+*
+* Return: depending on n value
+*/
+
+int _abs(int n)
+{
+if (n > 0)
+{
+return (n);
+}
+else if (n == 0)
+{
+return (0);
+}
+else
+{
+return (-n);
+}
+}
