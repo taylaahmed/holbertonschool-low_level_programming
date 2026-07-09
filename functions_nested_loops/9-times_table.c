107f25a25c2d80ef48ddf4dfc08ccaa8ef087362
taylaahmed <14827@holbertonstudents.com>
2026-07-09 18:13:00 +1000

    09

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -18,16 +18,18 @@ for (n = 0; n <= 9; n++)
 for (i = 0; i <= 9; i++)
 {
 product = n * i;
-if (product > 10)
+
+if (product >= 10)
 {
 _putchar((product / 10) + '0');
 _putchar((product % 10) + '0');
-_putchar(',');
-_putchar(' ');
 }
 else
 {
 _putchar(product + '0');
+}
+if (i != 9)
+{
 _putchar(',');
 _putchar(' ');
 }
