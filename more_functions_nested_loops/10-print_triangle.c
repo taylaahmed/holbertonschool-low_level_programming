d1a4da7c8d5d93e0882b3aab979decdfeba90fd4
tayla <14827@holbertonstudents.com>
2026-07-11 00:12:38 +1000

    10

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_triangle - check the code
+ * @size: check integer
+ *
+ * Return: Always 0.
+ */
+
+void print_triangle(int size)
+{
+int row;
+int space;
+int hash;
+
+if (size <= 0)
+{
+    _putchar('\n');
+}
+for (row = 1; row <= size; row++)
+{
+for (space = 0; space < size - row; space++)
+{
+    _putchar(' ');
+}
+for (hash = 0; hash < row; hash++)
+{
+    _putchar('#');
+}
+_putchar('\n');
+}
+
+}
