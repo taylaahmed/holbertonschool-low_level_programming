3ec742aa105fff3f979359f9a1d62bdc01e6ff3a
tayla <14827@holbertonstudents.com>
2026-07-11 00:14:03 +1000

    10

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -15,17 +15,19 @@ int hash;
 
 if (size <= 0)
 {
-    _putchar('\n');
+_putchar('\n');
 }
+
 for (row = 1; row <= size; row++)
 {
 for (space = 0; space < size - row; space++)
 {
-    _putchar(' ');
+_putchar(' ');
 }
+
 for (hash = 0; hash < row; hash++)
 {
-    _putchar('#');
+ _putchar('#');
 }
 _putchar('\n');
 }
