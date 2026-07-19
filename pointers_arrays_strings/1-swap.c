e9c3aecc3334a795a4f203638669de7aea2d01c5
taylaahmed <14827@holbertonstudents.com>
2026-07-19 13:59:15 +1000

    1-swap 01

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/*
+* swap_int - swap integers for *a and *b
+* @*a: variable 1
+* @*b: variable 2
+*/
+
+void swap_int(int *a, int *b)
+{
+int m;
+
+m = *a;
+*a = *b;
+*b = m;
+}
