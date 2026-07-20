d13253b0497d7a1aaab22d43e3d77c08d27e8947
taylaahmed <14827@holbertonstudents.com>
2026-07-20 17:52:07 +1000

    05

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -12,14 +12,21 @@
 char *_strstr(char *haystack, char *needle)
 {
 int i;
-int n = 0;
+int n;
 
 for (i = 0; haystack[i] != '\0'; i++)
 {
+n = 0;
 while (needle[n] != '\0' && haystack[i + n] == needle[n])
 {
-return (&needle[n]);
+n++;
 }
+
+if (needle[n] == '\0')
+{
+return (&haystack[i]);
+}
+
 }
 return (NULL);
 }
