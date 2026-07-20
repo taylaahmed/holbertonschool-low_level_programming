3e975920fe2f0e37452a746018abd82f403f4ad6
taylaahmed <14827@holbertonstudents.com>
2026-07-20 18:29:58 +1000

    08

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* print_chessboard - check code
+* @a: var
+*
+* Return: no return void
+*/
+
+void print_chessboard(char (*a)[8])
+{
+int row;
+int col;
+char c;
+
+for (row = 0; row < 8; row++)
+{
+for (col = 0; col < 8; col ++)
+{
+c = a[row][col];
+_putchar(c);
+}
+_putchar('\n');
+}
+}
