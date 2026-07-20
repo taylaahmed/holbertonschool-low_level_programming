a298824352a47d61e26623ae7f7e49f19d9a0581
taylaahmed <14827@holbertonstudents.com>
2026-07-20 16:10:39 +1000

    00

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* _memset - change 0x00 with 0x01
+* @s: var
+* @b: var
+* @n: var
+*
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int i; 
+
+for (i = 0; i <= n - 1; i++)
+{
+s[i] = b;
+}
+
+return (&s[i]);
+}
