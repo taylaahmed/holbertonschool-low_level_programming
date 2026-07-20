10243239cc002617da6e959a41a39e219d379d9d
taylaahmed <14827@holbertonstudents.com>
2026-07-20 23:08:26 +1000

    00

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+* create_array - check code
+* @size: int var
+* @c: char var
+*
+* Return: NULL or array
+*/
+char *create_array(unsigned int size, char c)
+{
+char *array;
+unsigned int i;
+array = malloc(size * sizeof(char));
+
+if (size == 0)
+{
+    return (NULL);
+}
+
+for (i = 0; i < size; i++)
+{
+    array[i] = c;
+
+}
+
+return (array);
+}
