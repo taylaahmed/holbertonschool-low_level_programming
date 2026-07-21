251b29fefb2768337133b38af907ec582ccf6b17
taylaahmed <14827@holbertonstudents.com>
2026-07-21 10:37:52 +1000

    00

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -13,21 +13,18 @@ char *create_array(unsigned int size, char c)
 {
 char *array;
 unsigned int i;
+
 array = malloc(size * sizeof(char));
 
-if (size == 0)
+if (size == 0 || array == NULL)
 {
 return (NULL);
 }
 
-if (array == NULL)
-return (NULL);
-
-for (i = 0; i < size; i++)
-{
-array[i] = c;
-
-}
+	for (i = 0; i < size; i++)
+	{
+	array[i] = c;
+	}
 
 return (array);
 }
---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+* create_array - check code
+* @size: int var
+* @c: char var
+*
+* Return: NULL or array
+*/
+
+void free_grid(int **grid, int height)
+{
+
+}
