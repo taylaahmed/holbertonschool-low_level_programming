5b073ca223ddb18322562a52dfc5daa898408429
taylaahmed <14827@holbertonstudents.com>
2026-07-21 12:03:49 +1000

    tabs

--- simple-calculator/calculator.c
+++ simple-calculator/calculator.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
-* main - function of quit calc
+* main - functions of a calculator (addition, subtration, multiplication and division)
 *
 *
 * Return: 0
@@ -9,87 +9,87 @@
 
 int main(void)
 {
-int n = 0; 
-int a = 0;
-int b = 0;
-int r;
-double d;
+	int n = 0; 
+	int a = 0;
+	int b = 0;
+	int r;
+	double d;
 
-printf("1) Add\n");
-printf("2) Subtract\n");
-printf("3) Multiply\n");
-printf("4) Divide\n");
-printf("0) Bye\n");
+	printf("1) Add\n");
+	printf("2) Subtract\n");
+	printf("3) Multiply\n");
+	printf("4) Divide\n");
+	printf("0) Bye\n");
 
-printf("Choice: ");
-scanf("%d", &n);
+	printf("Choice: ");
+	scanf("%d", &n);
 
-while (n != 0)
+	while (n != 0)
+	{
+		if (n < 0 || n >= 5)
+		{
+		printf("Invalid choice\n");
+		}
 
-{
-if (n < 0 || n >= 5)
-{
-printf("Invalid choice\n");
-}
+		if (n == 1)
+		{
+		printf("A: ");
+		scanf("%d", &a);
+		printf("B: ");
+		scanf("%d", &b);
 
-if (n == 1)
-{
-printf("A: ");
-scanf("%d", &a);
-printf("B: ");
-scanf("%d", &b);
+		r = (a + b);
+		printf("Result: %d\n", r);
+		}
 
-r = (a + b);
-printf("Result: %d\n", r);
-}
+		if (n == 2)
+		{
+		printf("A: ");
+		scanf("%d", &a);
+		printf("B: ");
+		scanf("%d", &b);
 
-if (n == 2)
-{
-printf("A: ");
-scanf("%d", &a);
-printf("B: ");
-scanf("%d", &b);
+		r = (a - b);
+		printf("Result: %d\n", r);
+		}
 
-r = (a - b);
-printf("Result: %d\n", r);
-}
+		if (n == 3)
+		{
+		printf("A: ");
+		scanf("%d", &a);
+		printf("B: ");
+		scanf("%d", &b);
 
-if (n == 3)
-{
-printf("A: ");
-scanf("%d", &a);
-printf("B: ");
-scanf("%d", &b);
+		r = (a * b);
+		printf("Result: %d\n", r);
+		}
 
-r = (a * b);
-printf("Result: %d\n", r);
-}
+		if (n == 4)
+		{
+		printf("A: ");
+		scanf("%d", &a);
+		printf("B: ");
+		scanf("%d", &b);
 
-if (n == 4)
-{
-printf("A: ");
-scanf("%d", &a);
-printf("B: ");
-scanf("%d", &b);
-if (b == 0)
-{
-printf("Error: division by zero\n");
-}
+		if (b == 0)
+		{
+		printf("Error: division by zero\n");
+		}
 
-else
-{
-d = ((float)a / b);
-printf("Result: %.1f\n", d);
-}
-}
+		else
+		{
+		d = ((float)a / b);
+		printf("Result: %.1f\n", d);
+		}
+		}
 
-printf("Choice: ");
-scanf("%d", &n);
-}
+		printf("Choice: ");
+		scanf("%d", &n);
+		}
 
-printf("Bye!\n");
+	printf("Bye!\n");
 
-return (0);
+	return (0);
 
 
 }
