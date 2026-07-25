fcb6f55afb7cd48afa9ab5bae96299823a7740f2
taylaahmed <14827@holbertonstudents.com>
2026-07-25 12:27:44 +1000

    dog 00

---
+++ structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifdef DOG_H
+#define DOG_H
+
+/**
+* main - function struct
+*
+* Return - none
+*/
+
+int main(void)
+{
+	struct dog
+	{
+		char *name;
+		float age;
+		char *owner;
+	};
+}
+
+#endif
