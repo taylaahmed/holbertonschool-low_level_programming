08a8e1a1c82b59c1fde5080d4904a86237e49d73
taylaahmed <14827@holbertonstudents.com>
2026-07-25 15:31:25 +1000

    free 00

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* free_dog - write function to free
+* @d: matrix info
+*
+* Return: none
+*/
+
+void free_dog(dog_t *d)
+{
+
+	free(d->owner);
+	free(d->name);
+
+	free(d);
+}
