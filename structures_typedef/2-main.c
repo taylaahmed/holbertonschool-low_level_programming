01e1eecc23b6b98bec0411f59d6755d0255e1004
taylaahmed <14827@holbertonstudents.com>
2026-07-25 13:36:15 +1000

    Print_Dog 00

---
+++ structures_typedef/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_dog(NULL);
+    return (0);
+}
