f50ad59f40d9a4412e76cbf42d089b5424251835
taylaahmed <14827@holbertonstudents.com>
2026-07-29 17:43:56 +1000

    Vari 01-01

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -16,16 +16,13 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list print;
 	unsigned int i;
 
-	if (separator == NULL)
-		return;
-
 	va_start(print, n);
 
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(print, int));
 
-		if (i < (n - 1))
+		if (i < (n - 1) && separator != NULL)
 		{
 			printf("%s", separator);
 		}
