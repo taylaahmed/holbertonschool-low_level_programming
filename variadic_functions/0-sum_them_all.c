ff22a087983d35904bc816e75c45ef2b4d5e1a44
taylaahmed <14827@holbertonstudents.com>
2026-07-29 16:59:41 +1000

    Vari 00-00

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,31 @@
+#include "variadic_functions.h"
+#include <stdlib.h>
+#include <stdarg.h>
+
+/**
+ * sum_then_all - check code
+ * @n: unsigned constant int
+ *
+ * Return: 0 or n
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list va;
+	unsigned int i = 0;
+	int sum = 0;
+
+	if (n == 0)
+		return (0);
+
+	va_start(va, n);
+
+	while (i < n)
+	{
+		sum += va_arg(va, int);
+		i++;
+	}
+
+	va_end(va);
+	return (sum);
+}
---
+++ variadic_functions/variadic_functions.h
@@ -0,0 +1,6 @@
+#define VARIADIC_FUNCTIONS_H
+#ifndef VARIADIC_FUNCTION_H
+
+int sum_them_all(const unsigned int n, ...);
+
+#endif
