16527de441ecc55786eb9e78d1119ffbb6ef346b
taylaahmed <14827@holbertonstudents.com>
2026-07-29 17:39:19 +1000

    vari 01-00

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,38 @@
+#include "variadic_functions.h"
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_numbers - check code
+ * @separator: ", "
+ * @n: number of arguments
+ *
+ * Return: void
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list print;
+	unsigned int i;
+
+	if (separator == NULL)
+		return;
+
+	va_start(print, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(print, int));
+
+		if (i < (n - 1))
+		{
+			printf("%s", separator);
+		}
+	}
+
+	va_end(print);
+
+	printf("\n");
+
+}
