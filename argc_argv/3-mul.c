ab703bb2219853599f39a6a9c58474f35b61976e
taylaahmed <14827@holbertonstudents.com>
2026-07-31 11:33:33 +1000

    Argv 03-01

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -12,18 +12,15 @@
 
 int main(int argc, char *argv[])
 {
-	int a;
-	int b;
 
-	if (argc == 1 || argc > 3)
+	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
-
-	a = atoi(argv[1]);
-	b = atoi(argv[2]);
-
-	printf("%d\n", (a * b));
-	return (0);
+	else
+	{
+		printf("%d\n", (atoi(argv[1]) * atoi(argv[2])));
+		return (0);
+	}
 }
