6df24fa3076cd53da3698b63271492ddc1b4f315
taylaahmed <14827@holbertonstudents.com>
2026-08-05 15:29:26 +1000

    function used 02

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -15,13 +15,16 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *temp;
+	int i = 0;
 
 	temp = malloc(sizeof(list_t));
 	if (temp == NULL)
 		return (NULL);
 
 	temp->str = strdup(str);
-	temp->len = strlen(str);
+	while (str[i] != '\0')
+		i++;
+	temp->len = i;
 	temp->next = *head;
 
 	*head = temp;
