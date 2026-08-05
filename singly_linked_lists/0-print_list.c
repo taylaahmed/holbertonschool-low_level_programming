bde0cda8fc22ae5be0ac7ba64f7078801efb9cd3
taylaahmed <14827@holbertonstudents.com>
2026-08-05 14:33:23 +1000

    Linkedlist - 00

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ *
+ *
+ *
+ *
+ */
+
+size_t print_list(const list_t *h)
+{
+	int count = 0;
+	const list_t *current = h;
+
+	while (current != NULL)
+	{
+		if (current->str == NULL)
+			printf("[0] (nil)");
+		else
+			printf("[%d] %s", current->len, current->str);
+		current = current->next;
+		count++;
+	}
+	return (count);
+}
