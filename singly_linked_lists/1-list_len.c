c54fdf9fd9e90b717e2ad8310983dc82b638d116
taylaahmed <14827@holbertonstudents.com>
2026-08-05 14:50:03 +1000

    linked list 01

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+/**
+ * list_len - return value of list
+ * @h: the linked list
+ *
+ * Return: count of list elements
+ */
+
+size_t list_len(const list_t *h)
+{
+	int count;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
