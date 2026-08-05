9f4a91e84f141aa247d4c6b99a71a40f61bd37fd
taylaahmed <14827@holbertonstudents.com>
2026-08-05 14:39:54 +1000

    betty-complience 00

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -4,10 +4,10 @@
 #include <stddef.h>
 
 /**
+ * print_list - print the given linked list
+ * @h: linked list
  *
- *
- *
- *
+ * Return: value of things in linked list
  */
 
 size_t print_list(const list_t *h)
@@ -18,7 +18,7 @@ size_t print_list(const list_t *h)
 	while (current != NULL)
 	{
 		if (current->str == NULL)
-			printf("[0] (nil)i\n");
+			printf("[0] (nil)\n");
 		else
 			printf("[%d] %s\n", current->len, current->str);
 		current = current->next;
