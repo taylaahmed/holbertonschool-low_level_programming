4bc25b008659a32cad855e4298684301575af769
taylaahmed <14827@holbertonstudents.com>
2026-08-10 18:57:43 +1000

    add node at end

---
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * add_dnodeint_end - place doubly linked node at end
+ * @head: first node
+ * @n: inside of node
+ *
+ * Return: temp
+ */
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *temp;
+	dlistint_t *tail;
+
+	tail = *head;
+
+	temp = malloc(sizeof(dlistint_t));
+	if (temp == NULL)
+		return (NULL);
+
+	if (*head == NULL)
+	{
+		*head = temp;
+		return (temp);
+	}
+
+	while (tail->next != NULL)
+	{
+		tail = tail->next;
+	}
+
+	temp->n = n;
+	temp->next = NULL;
+	temp->prev = tail;
+
+	tail->next = temp;
+	return (temp);
+}
