748fb1ea763c3bbf61c5187b8094af2444da72e0
taylaahmed <14827@holbertonstudents.com>
2026-08-10 18:18:33 +1000

    betty 00

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -3,15 +3,23 @@
 #include <stdio.h>
 #include "lists.h"
 
+/**
+ * print_dlistint - printing a doubly linked list
+ * @h: where the nodes are stored
+ *
+ * Return: number of nodes
+ */
+
 size_t print_dlistint(const dlistint_t *h)
 {
-    int count = 0;
+	int count = 0;
 
-    	while (h != NULL)
+	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
 		count++;
 	}
+
 	return (count);
 }
