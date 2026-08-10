2462f3ecf638fa38f3c067b3f1e90786bd26040f
taylaahmed <14827@holbertonstudents.com>
2026-08-10 18:11:04 +1000

    print double linked list

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+size_t print_dlistint(const dlistint_t *h)
+{
+    int count = 0;
+
+    	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
