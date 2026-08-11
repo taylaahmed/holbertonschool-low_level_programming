cb7f765ca91e9c438e36245c0e6fde765c82af4a
taylaahmed <14827@holbertonstudents.com>
2026-08-11 20:14:49 +1000

    05 - double link

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -16,15 +16,12 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 
 	unsigned int n = 0;
 
-	while (head != NULL)
-		n++;
-
-	if (n > index)
+	if (head == NULL)
 		return (NULL);
 
-	while (n < index)
+	while (head != NULL && n < index)
 	{
-		
+
 		head = head->next;
 		n++;
 	}
