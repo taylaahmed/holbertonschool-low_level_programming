ddec852ee4fd6c28e318e2de5238d217b1d1ae11
taylaahmed <14827@holbertonstudents.com>
2026-08-14 16:58:27 +1000

    first try 4-get

---
+++ hash_tables/4-hash_table_get.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - retrieve info from hash
+ * @ht: my hash table
+ * @key: where I want ot reteive info
+ *
+ * Return: the value at key
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *current;
+	unsigned long int index;
+
+	index = key_index((unsigned char *)key, ht->size);
+	current = ht->array[index];
+
+	while (current != NULL)
+	{
+		if (strcmp(current->key, key) == 0)
+		{
+			return (current->value);
+		}
+	current = current->next;
+	}
+
+	return (NULL);
+}
