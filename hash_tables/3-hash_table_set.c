9bb489f9d42c3b0af2cb2eb8050e0ae9ce48eb01
taylaahmed <14827@holbertonstudents.com>
2026-08-14 16:41:21 +1000

    betty complient

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -3,65 +3,84 @@
 #include <stdio.h>
 #include "hash_tables.h"
 
-/*
-* hash_table_set - setting the nodes of a hash table
-* @ht: the table
-* @key: where in the array
-* @value: the stored value within the node
-*
-* Return: 1 or 0
-*/
+/**
+ * hash_table_set - setting the nodes of a hash table
+ * @ht: the table
+ * @key: where in the array
+ * @value: the stored value within the node
+ *
+ * Return: 1 or 0
+ */
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-    unsigned long int index;
-    hash_node_t *new_node;
-    hash_node_t *current;
-    char *new_value;
+	unsigned long int index;
+	hash_node_t *current;
+	char *new_value;
+	int i = 0;
 
-    if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
-        return (0);
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+		return (0);
+	index = key_index((unsigned char *)key, ht->size);
+	current = ht->array[index];
 
-    index = key_index((unsigned char *)key, ht->size);
-    current = ht->array[index];
+	while (current != NULL)
+	{
+		if (strcmp(current->key, key) == 0)
+		{
+			new_value = strdup(value);
+			if (new_value == NULL)
+			return (0);
 
-    while (current != NULL)
-    {
-        if (strcmp(current->key, key) == 0)
-        {
-            new_value = strdup(value);
-            if (new_value == NULL)
-                return (0);
+			free(current->value);
+			current->value = new_value;
+			return (1);
+		}
+		current = current->next;
+	}
 
-            free(current->value);
-            current->value = new_value;
+	i = create_new_node(ht, key, value);
 
-            return (1);
-        }
-        current = current->next;
-    }
+	return (i);
+}
 
-    /* Create a new node */
-    new_node = malloc(sizeof(hash_node_t));
-    if (new_node == NULL)
-        return (0);
+/**
+ * create_new_node - creating a new node
+ * @ht: the whole hash table thing
+ * @value: the value being stored within node value
+ * @key: the code at which the node is being stored
+ *
+ * Return: 1 or 0 to show success/failiure
+*/
 
-    new_node->key = strdup(key);
-    if (new_node->key == NULL)
-    {
-        free(new_node);
-        return (0);
-    }
+int create_new_node(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *new_node;
+	unsigned long int index;
 
-    new_node->value = strdup(value);
-    if (new_node->value == NULL)
-    {
-        free(new_node->key);
-        free(new_node);
-        return (0);
-    }
-    new_node->next = ht->array[index];
-    ht->array[index] = new_node;
+	index = key_index((unsigned char *)key, ht->size);
 
-    return (1);
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+		return (0);
+
+	new_node->key = strdup(key);
+	if (new_node->key == NULL)
+	{
+		free(new_node);
+		return (0);
+	}
+
+	new_node->value = strdup(value);
+	if (new_node->value == NULL)
+	{
+		free(new_node->key);
+		free(new_node);
+		return (0);
+	}
+
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+
+	return (1);
 }
