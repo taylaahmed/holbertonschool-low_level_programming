668482f8730f0c3c5f02fdbd871bc313586ccec8
taylaahmed <14827@holbertonstudents.com>
2026-08-14 17:54:15 +1000

    betty complience

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -4,39 +4,40 @@
 #include "hash_tables.h"
 
 /**
- * 
- * 
- * 
+ * hash_table_delete - free
+ * @ht: the hash table
+ *
+ * Return: void
  */
 
 void hash_table_delete(hash_table_t *ht)
 {
-    unsigned int size;
-    unsigned int i;
-    hash_node_t *current;
-    hash_node_t *delete;
+	unsigned int size;
+	unsigned int i;
+	hash_node_t *current;
+	hash_node_t *delete;
 
-    if (ht == NULL)
-    {
-        return;
-    }
+	if (ht == NULL)
+	{
+		return;
+	}
 
-    size = ht->size;
+	size = ht->size;
 
-    for (i = 0; i < size; i++)
-    {
-        current = ht->array[i];
-        while (current != NULL)
-        {
-            delete = current;
-            current = current->next;
+	for (i = 0; i < size; i++)
+	{
+		current = ht->array[i];
+		while (current != NULL)
+		{
+			delete = current;
+			current = current->next;
 
-            free(delete->key);
-            free(delete->value);
-            free(delete);
-        }
-    }
+			free(delete->key);
+			free(delete->value);
+			free(delete);
+		}
+	}
 
-    free(ht->array);
-    free(ht);
+	free(ht->array);
+	free(ht);
 }
