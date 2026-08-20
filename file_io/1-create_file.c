0df55d97ddc2bb18d10e3673b1a9b10a37ddd6a6
taylaahmed <14827@holbertonstudents.com>
2026-08-20 20:19:36 +1000

    try 01 01

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -23,7 +23,7 @@ int create_file(const char *filename, char *text_content)
 
 	fd = open(filename, O_CREAT | O_WRONLY, 600);
 	written = write(fd, text_content, strlen(text_content));
-	if (written != -1)
+	if (written == -1)
 	{
 		return (-1);
 	}
